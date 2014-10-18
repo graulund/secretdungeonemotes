4761bc16c6b21a03533f72785c08d1c120cfab18
Andy Graulund <andreas@graulund.com>
2014-10-18 15:17:47 +0200

    New file structure with images internal to project; JSON file now has base64 image data, attainable with the included JSON file generator. Run this generator whenever you want to add a new emoticon; do not edit it manually.

---
+++ generateemotelist.php
@@ -0,0 +1,61 @@
+<?php
+	// SECRET DUNGEON EMOTES
+	// Write JSON emote list with base64 data for the images
+
+	// (Run this file in the project root dir in order to generate the new JSON file)
+
+	define('IMAGES_DIR', 'images');
+	define('JSON_FILENAME', 'dungeonemotes.json');
+	define('JSONP_METHOD_NAME', 'sde_jsonp_static');
+
+	$pwd = realpath('.');
+	$imagesDir = $pwd . DIRECTORY_SEPARATOR . IMAGES_DIR;
+	$jsonFilename = $pwd . DIRECTORY_SEPARATOR . JSON_FILENAME;
+	$output = array();
+
+	echo "SECRET DUNGEON EMOTES JSON GENERATOR\n";
+
+	foreach(scandir($imagesDir) as $filename){
+
+		// Only image files that support transparency
+		if(preg_match('/\.(png|gif)$/', $filename)){
+
+			// Full path to this image file
+			$path = $imagesDir . DIRECTORY_SEPARATOR . $filename;
+
+			// Getting data about image
+			$imgData = getimagesize($path);
+			$width   = $imgData[0];
+			$height  = $imgData[1];
+			$mime    = $imgData['mime'];
+
+			// Getting emoticon name
+			$segs    = explode('.', $filename, 2);
+			$name    = $segs[0];
+
+			echo 'Getting data for ' . $name . '...';
+
+			// Output
+			$output[] = array(
+				'name'   => $name,
+				'width'  => $width,
+				'height' => $height,
+				'url'    => 'data:' . $mime . ';base64,' .
+				            urlencode(base64_encode(file_get_contents($path)))
+			);
+
+			echo "Done!\n";
+		}
+	}
+
+	echo 'Writing to file...';
+
+	// Write to the file!
+	file_put_contents(
+		$jsonFilename,
+		JSONP_METHOD_NAME . '(' .
+			json_encode($output) .
+		')'
+	);
+
+	echo "Done!\n";
