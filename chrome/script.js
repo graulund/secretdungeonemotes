c6a1ca19649868a1b0fa5ff3dbd8b0eb3b93d86d
Andy Graulund <andy@graulund.com>
2015-04-23 19:41:09 +0200

    Added uncompressed Chrome extension

---
+++ chrome/script.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        Secret Dungeon Emotes
+// @namespace   graulund.github.io
+// @description A set of cool (in-joke) emoticons that you can use anywhere on Twitch.
+
+// @include	*.twitch.tv/*
+// @exclude api.twitch.tv/*
+
+// @version 2.0
+// @updateURL http://graulund.github.io/secretdungeonemotes/dungeonemotes.user.js
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+
+function sdeLoad()
+{
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "//graulund.github.io/secretdungeonemotes/sde.js";
+	document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+sdeLoad();
